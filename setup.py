c16d54dd8d1da015080a3414ae1484f8d39ab787
Ben Cole <wengole@gmail.com>
2019-05-16 13:25:37 +0100

    2.12.0 of factoryboy moves `extract_dict`

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import re
 from setuptools import find_packages, setup
 
 install_requires = [
-    'factory-boy>=2.8.0',
+    'factory-boy>=2.8.0,<2.12.0',
     'wagtail>=1.12',
     'flatdict>=3.0.1',
 ]
