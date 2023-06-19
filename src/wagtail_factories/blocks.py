e8e972b40566c87a780ab833eb172cf148b8bb56
Brad Morton <bradley.morton@nhschoices.net>
2023-06-19 20:52:00 +0100

    remove dupe streamblock and streamsubblock

--- src/wagtail_factories/blocks.py
+++ src/wagtail_factories/blocks.py
@@ -95,60 +95,6 @@ class ListBlockFactory(factory.SubFactory):
 
         return retval
 
-
-class StreamBlockSubFactory(factory.SubFactory):
-
-    def __call__(self, **kwargs):
-        return self.generate(None, kwargs)
-
-    def generate(self, step, params):
-        result = defaultdict(lambda: defaultdict(lambda: defaultdict()))
-        subfactory = self.get_factory()
-
-        for key, value in params.items():
-            parts = key.split('__', 2)
-            index = parts[0]
-            if index.isdigit():
-                index = int(index)
-            else:
-                continue
-
-            block_name = parts[1]
-
-            if len(parts) == 2:
-                result[index][block_name][block_name] = value
-            if len(parts) == 3:
-                param = parts[2]
-                result[index][block_name][block_name + "__" + param] = value
-
-        retval = []
-        for index, block_items in sorted(result.items()):
-            for block_name, block_params in block_items.items():
-                block_params['_block_name'] = block_name
-                step_builder = factory.builder.StepBuilder(
-                    subfactory._meta,
-                    block_params,
-                    "build"
-                )
-                built = step_builder.build()
-                retval.append((block_name, built))
-
-        return blocks.StreamValue(subfactory._meta.model(), retval)
-
-
-class StreamBlockFactory(factory.Factory):
-
-    @classmethod
-    def _build(cls, model_class, *args, **kwargs):
-        block = model_class()
-        block_name = kwargs['_block_name']
-        block_params = kwargs.get(block_name, None)
-        return block.to_python([{"type": block_name, "value": block_params}])[0].value
-
-    @classmethod
-    def _create(cls, model_class, *args, **kwargs):
-        return cls._build(model_class, *args, **kwargs)
-
 class BlockFactory(factory.Factory):
     class Meta:
         abstract = True
